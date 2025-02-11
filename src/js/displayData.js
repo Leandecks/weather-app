8ba96cef08daa5eb1247221d01b8dd174bb1b5ef
Leandecks <leandrogridelli@gmail.com>
2025-02-11 20:37:06 +0100

    Data to display moved to new file

---
+++ src/js/displayData.js
@@ -0,0 +1,79 @@
+function displayData(data) {
+  const container = document.querySelector("#weather");
+  const city = container.querySelector("#city");
+  const fullplace = container.querySelector("#fullplace");
+  const coords = container.querySelector("#coords");
+  const description = container.querySelector("#description");
+  const alerts = container.querySelector("#alerts");
+  const conditions = container.querySelector("#conditions");
+  const temperature = container.querySelectorAll(".temperature");
+  const feelsLike = container.querySelector("#feels-like");
+  const dew = container.querySelector("#dew");
+  const humidity = container.querySelector("#humidity");
+  const rainFall = container.querySelector("#rain-fall");
+  const rainProbability = container.querySelector("#rain-probability");
+  const rainType = container.querySelector("#rain-type");
+  const pressure = container.querySelector("#pressure");
+  const snowFall = container.querySelector("#snow-fall");
+  const snowDepth = container.querySelector("#snow-depth");
+  const sunrise = container.querySelector("#sunrise");
+  const sunset = container.querySelector("#sunset");
+  const windDirection = container.querySelector("#wind-direction");
+  const windSpeed = container.querySelector("#wind-speed");
+  const windSpeedKmh = container.querySelector("#wind-speed-kmh");
+  const windCurrentSpeed = container.querySelector("#wind-current-speed");
+  const windCurrentSpeedKmh = container.querySelector(
+    "#wind-current-speed-kmh",
+  );
+  const solarEnergy = container.querySelector("#solar-energy");
+  const visibility = container.querySelector("#visibility");
+
+  const time = document.querySelector("#time");
+
+  city.textContent = data.place;
+  fullplace.textContent = data.fullPlace;
+  coords.textContent = `${data.coordinates.latitude}, ${data.coordinates.longitude}`;
+  description.textContent = data.description;
+
+  if (data.alerts.length === 0) {
+    const alert = document.createElement("p");
+    alert.textContent = "There are no alerts.";
+    alerts.appendChild(alert);
+  } else {
+    for (let i = 0; i < data.alerts.length; i++) {
+      const alert = document.createElement("p");
+      alert.textContent = data.alerts[i].event;
+      alerts.appendChild(alert);
+    }
+  }
+
+  conditions.textContent = data.current.conditions;
+
+  [...temperature].forEach((temp) => {
+    console.log(temp);
+    temp.textContent = data.current.temperature;
+  });
+
+  feelsLike.textContent = data.current.feelsLike;
+  dew.textContent = data.current.dew;
+  humidity.textContent = data.current.humidity;
+  rainFall.textContent = data.current.precipitation.fall;
+  rainProbability.textContent = data.current.precipitation.probability;
+  rainType.textContent = data.current.precipitation.type;
+  pressure.textContent = data.current.pressure;
+  snowFall.textContent = data.current.snow.fall;
+  snowDepth.textContent = data.current.snow.depth;
+  sunrise.textContent = data.current.sunrise;
+  sunset.textContent = data.current.sunset;
+  windDirection.textContent = data.current.wind.direction;
+  windSpeed.textContent = data.current.wind.speed;
+  windSpeedKmh.textContent = data.current.wind.speedKmh;
+  windCurrentSpeed.textContent = data.current.wind.currentSpeed;
+  windCurrentSpeedKmh.textContent = data.current.wind.currentSpeedKmh;
+  solarEnergy.textContent = data.current.solarEnergy;
+  visibility.textContent = data.current.visibility;
+
+  time.textContent = data.current.time;
+}
+
+export default displayData;
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import "../index.html";
 import "../css/style.css";
 import getLocationData from "./getLocationData";
 import processLocationData from "./processLocationData";
+import displayData from "./displayData";
 
 document.addEventListener("DOMContentLoaded", () => {
   const inputField = document.querySelector("#location");
@@ -27,25 +28,5 @@ async function request(place) {
     console.log(object);
 
     displayData(object);
-  }
-}
-
-function displayData(data) {
-  const container = document.querySelector("#weather");
-  const city = container.querySelector("#city");
-  const fullplace = container.querySelector("#fullplace");
-  const coords = container.querySelector("#coords");
-  const description = container.querySelector("#description");
-  const alerts = container.querySelector("#alerts");
-
-  city.textContent = data.place;
-  fullplace.textContent = data.fullPlace;
-  coords.textContent = `${data.coordinates.latitude} ${data.coordinates.longitude}`;
-  description.textContent = data.description;
-
-  for (let i = 0; i < data.alerts.length; i++) {
-    const alert = document.createElement("p");
-    alert.textContent = data.alerts[i].event;
-    alerts.appendChild(alert);
   }
 }
