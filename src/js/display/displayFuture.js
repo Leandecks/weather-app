6c623d4db72d763ed434e47f3819bbedc2bffddb
Leandecks <leandrogridelli@proton.me>
2025-06-18 17:32:06 +0200

    Next 3 days display

--- src/js/display/displayFuture.js
+++ src/js/display/displayFuture.js
@@ -4,6 +4,23 @@ function displayFuture(data) {
   const container = document.querySelector("#next-days");
   const day2Name = container.querySelector("#day2>h4");
   const day3Name = container.querySelector("#day3>h4");
+  const icons = container.querySelectorAll("i");
+  const conditions = container.querySelectorAll(".conditions");
+  const temperature = container.querySelectorAll(".temperature");
+  const minmax = container.querySelectorAll(".minmax");
+  const precipitation = container.querySelectorAll(".precipitation");
+
+  for (let i = 0; i < icons.length; i++) {
+    icons[i].classList = "";
+    icons[i].classList.add("nf");
+    icons[i].classList.add(data.future[i].icon);
+
+    conditions[i].textContent = data.future[i].conditions;
+    temperature[i].textContent = data.future[i].temperature;
+    minmax[i].textContent =
+      data.future[i].minTemperature + " ~ " + data.future[i].maxTemperature;
+    precipitation[i].textContent = data.future[i].rainChance + " %";
+  }
 
   day2Name.textContent = getWeekday(2);
   day3Name.textContent = getWeekday(3);
