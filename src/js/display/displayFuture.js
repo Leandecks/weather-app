d9ed2b320a3bcbbe1ad045f9230093d0c2d839bf
Leandecks <leandrogridelli@proton.me>
2025-08-10 14:59:53 +0200

    future conditions redesign + minmax and rain colors

--- src/js/display/displayFuture.js
+++ src/js/display/displayFuture.js
@@ -10,19 +10,28 @@ function displayFuture(data) {
   const minmax = container.querySelectorAll(".minmax");
   const precipitation = container.querySelectorAll(".precipitation");
 
+  const RAIN_COLOR = "#11dfff";
+  const BASE_COLOR = "#fefae0";
+
   for (let i = 0; i < icons.length; i++) {
+    const minTemperature = minmax[i].querySelector(".min-temp");
+    const maxTemperature = minmax[i].querySelector(".max-temp");
+
     icons[i].classList = "";
     icons[i].classList.add("nf");
     icons[i].classList.add(data.future[i].icon);
 
     conditions[i].textContent = data.future[i].conditions;
-    temperature[i].textContent = data.future[i].temperature + " °C";
-    minmax[i].textContent =
-      data.future[i].minTemperature +
-      " °C ~ " +
-      data.future[i].maxTemperature +
-      " °C";
+    temperature[i].textContent = "Ø of " + data.future[i].temperature + " °C";
+    minTemperature.textContent = data.future[i].minTemperature + " °C";
+    maxTemperature.textContent = data.future[i].maxTemperature + " °C";
     precipitation[i].textContent = data.future[i].rainChance + " % rain chance";
+
+    if (data.future[i].rainChance >= 1) {
+      precipitation[i].style.backgroundColor = RAIN_COLOR;
+    } else {
+      precipitation[i].style.backgroundColor = BASE_COLOR;
+    }
   }
 
   day2Name.textContent = getWeekday(2);
